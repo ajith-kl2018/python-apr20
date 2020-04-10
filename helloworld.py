62f7008a2517ebd089cf2fa7f1df07fae6a295f6
Ajith Kumar <ajith@knowesis.com>
2020-04-10 15:12:32 +1000

    Created Helloworld

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# This is my first hello world!! program in python. Created for a Python class
+# @Created By Ajith
+# 
+
+#Below line prints hello world in sysout/screen
+print("Hello Kuala Lumpur!!!")
+
+
