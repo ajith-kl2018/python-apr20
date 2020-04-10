525aa6819b0edd412bc0f0bb5e49d2954d6802ef
Ajith Kumar <ajith@knowesis.com>
2020-04-10 16:29:54 +1000

    Finished String functions

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,30 @@
 # 
 
 #Below line prints hello world in sysout/screen
-print("Hello World!!!")
+#print("Hello World!!!")
+
+#Ask user their first name
+#firstName = input("What is your First Name?")
+
+#Ask user their last name
+#lastName = input("What is your Last Name?")
+
+
+#Print the user what they typed
+#print("Hi!, " + firstName + " " + lastName)
+
+#print("Hi!, " + firstName.lower() + " " + lastName.lower())
+
+#print("Hi!, " + firstName.upper() + " " + lastName.upper())
+
+##Ask user to enter the age for testing number data type
+age = input("What is your age?")
+print(age)
+
+### I am changing the value of name to a default value
+#name = "Python"
+
+
+
 
 
